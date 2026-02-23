921df824ef7f841435c780a4d26c7ef9940d3e08
Naitikk <Naetiksoni.08@gmail.com>
2026-02-23 23:49:00 +0530

    web request

--- src/web_request.ts
+++ src/web_request.ts
@@ -1,6 +1,20 @@
 import axios, { AxiosResponse } from "axios";
 
 
+// Basically, I was getting an error when I was importing xeo's response. The reason was thatExios is basically a functionality, while Exios response is basically a type which is present inside Exios only.So, when I was importing the first code, basically what was happening was that I was getting this error that I can not import a functionality as well as type both at the same time from one statement only. Now, the fix was simple: I went to my typescript, and my strict mode was true, which is verbity module syntax.
+//  It was true, and when I false it, then I stopped getting this error.But basically, the simple meaning of this
+//  error was that I cannot import a type as well as a functionality that is exios and a type is exiosResponse 
+// together, both at the same time. For example, if my verbatim module syntax is true, that means my TypeScript
+//  is running in strict mode, so basically I just simply do this: import the functionality of exios and my Type 
+// explicitly. I would have mentioned that import Type exiosResponse from exios, so I write separately that,
+//  'That's it, verbatimmodulesyntax is true; then this is it.'
+
+// import axios from 'axios';
+// import type { AxiosResponse } from "axios";
+// when verbitmodulesyntax=true
+
+
+
 interface Todo {
     userId: number;
     id: number;
@@ -8,7 +22,7 @@ interface Todo {
     completed: boolean;
 }
 
-const fetchData = async () => {
+const axiosfetchData = async () => {
     try {
         const response: AxiosResponse<Todo> =
             await axios.get("https://jsonplaceholder.typicode.com/todos/1");
@@ -24,7 +38,7 @@ const fetchData = async () => {
         }
     }
 };
-fetchData();
+axiosfetchData();
 
 
 
